9568ab198ca980867ff11f26f209e69e24a15823
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-04-04 17:23:12 -0600

    Added add and remove friend user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,5 +44,33 @@ module.exports = {
                 console.log(err);
                 res.status(500).json(err);
             })
+    },
+    addFriend(req, res) {
+        console.log("You are adding a friend");
+        console.log(req.body);
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { addToSet: { friends: req.body }},
+            { new: true }
+        )
+            .then((user) => 
+                !user
+                    ? res.status(404).json({ message: "No user found."})
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: { friendId: req.params.friendId }}},
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user found."})
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     }
 }
--- models/Reaction.js
+++ models/Reaction.js
@@ -1,5 +1,4 @@
 const { Schema, Types } = require("mongoose");
-const { stringify } = require("querystring");
 
 const reactionSchema = new Schema(
     {
