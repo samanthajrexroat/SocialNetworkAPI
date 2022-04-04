e9ecd88e5499e6904bd009537a14e62bc522d741
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-04-04 15:50:56 -0600

    added models, server, and some route boilerplate

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    },
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
